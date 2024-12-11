849b131e666a8823838196127ff234264e75de5a
YashHaritash <yashharitash4@gmail.com>
2024-12-11 16:15:51 +0530

    First Commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// Replace <repository-name> with the name of your GitHub repository
 export default defineConfig({
   plugins: [react()],
-})
+  base: 'tryGitPages1', 
+});
